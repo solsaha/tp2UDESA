44eb16c1e769b5a9da46f0bf0c65c65855d1e33f
solsaha <ssaha@udesa.edu.ar>
2021-04-11 12:39:00 -0300

    listado

---
+++ proyectoUDESA/data/buzos.js
@@ -0,0 +1,96 @@
+let buzos = {
+    lista: [
+        {
+            id:1,
+            nombre: "Buzo Kauri",
+            marca: "SAFUL",
+            talle: "1 , 2 , 3",
+            precio: 510,
+            //stock: ,
+            imagen:"/img/acdc."
+        },
+        {
+            id:2,
+            nombre: "Buzo Gema",
+            marca: "SAFUL",
+            talle: "1 , 2 , 3",
+            precio: 490,
+            //stock: ,
+            imagen:"/img/acdc."
+        },
+        {
+            id:3,
+            nombre: "Buzo Jade",
+            marca: "SAFUL",
+            talle: "1 , 2 , 3",
+            precio: 500,
+            //stock: ,
+            imagen:"/img/acdc."
+        },
+        {
+            id:4,
+            nombre: "Buzo Agata",
+            marca: "SAFUL",
+            talle: "1 , 2 , 3",
+            precio: 460,
+            //stock: ,
+            imagen:"/img/acdc."
+        },
+        {
+            id:5,
+            nombre: "Buzo Ambar",
+            marca: "SAFUL",
+            talle: "1 , 2 , 3",
+            precio: 450,
+            //stock: ,
+            imagen:"/img/acdc."
+        },
+        {
+            id:6,
+            nombre: "Buzo Arte",
+            marca: "SAFUL",
+            talle: "1 , 2 , 3",
+            precio: 400,
+            //stock: ,
+            imagen:"/img/acdc."
+        },
+        {
+            id:7,
+            nombre: "Buzo Bonnie",
+            marca: "SAFUL",
+            talle: "1 , 2 , 3",
+            precio: 380,
+            //stock: ,
+            imagen:"/img/acdc."
+        },
+        {
+            id:8,
+            nombre: "Buzo Diana",
+            marca: "SAFUL",
+            talle: "1 , 2 , 3",
+            precio: 360,
+            //stock: ,
+            imagen:"/img/acdc."
+        },
+        {
+            id:9,
+            nombre: "Buzo Jackie",
+            marca: "SAFUL",
+            talle: "1 , 2 , 3",
+            precio: 330,
+            //stock: ,
+            imagen:"/img/acdc."
+        },
+        {
+            id:10,
+            nombre: "Buzo Star",
+            marca: "SAFUL",
+            talle: "1 , 2 , 3",
+            precio: 350,
+            //stock: ,
+            imagen:"/img/acdc."
+        },
+    ]
+}
+
+module.exports = buzos;
