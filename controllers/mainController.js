d59df5255337d89130a92c1427e577dc26a6b3d5
solsaha <ssaha@udesa.edu.ar>
2021-05-26 13:06:43 -0300

    buzo

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,7 +3,7 @@ const db = require('../database/models');
 const mainController = {
     index: function(req, res){
         
-        db.Buzos.findAll()
+        db.Buzo.findAll()
             .then( data => {
                 return res.render('index', { buzos: data })
             })
