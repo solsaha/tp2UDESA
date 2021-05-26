5522ce208340c7f15af084612333d1d09652acd3
magacarolina <mparra@udesa.edu.ar>
2021-05-26 13:11:09 -0300

    main controller productos

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,9 +3,9 @@ const db = require('../database/models');
 const mainController = {
     index: function(req, res){
         
-        db.Buzo.findAll()
+        db.Productos.findAll()
             .then( data => {
-                return res.render('index', { buzos: data })
+                return res.render('index', { buzos : data })
             })
             .catch(error =>{
                 console.log(error);
