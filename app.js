8b35bb1c6c8b0a4ab4a6472359d174c872244a21
solsaha <ssaha@udesa.edu.ar>
2021-06-02 13:10:26 -0300

    cambios

--- app.js
+++ app.js
@@ -24,24 +24,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(session(
-//   { secret:'proyectoIntegrador',
-//     resave: false,
-//     saveUninitialized: true }
-// ));
+ /* app.use(session(
+  { secret:'proyectoIntegrador',
+    resave: false,
+    saveUninitialized: true }
+ )); */
 
-// // Antes de las rutas. Dejar disponible datos de sessión para todas las vistas
-// app.use(function(req, res, next){
-//   console.log('En session middleware');
-//   console.log(req.session.user);
-//   if(req.session.user != undefined){
-//     res.locals = req.session.user;
-//     console.log("entre en locals: ");
-//     console.log(res.locals);
-//     return next();
-//   } 
-//   return next(); //Clave para que el proceso siga adelante.  
-// })
+ // Antes de las rutas. Dejar disponible datos de sessión para todas las vistas
+ app.use(function(req, res, next){
+   console.log('En session middleware');
+  console.log(req.session.user);
+  if(req.session.user != undefined){
+     res.locals = req.session.user;
+     console.log("entre en locals: ");
+    console.log(res.locals);
+    return next();
+  } 
+   return next(); //Clave para que el proceso siga adelante.  
+ })
 
  //Gestionar la coockie.
  app.use(function(req, res, next){
--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,30 +1,3 @@
-// const bcrypt = require('bcryptjs');
-// const db = require('../database/models');
-// const op = db.Sequelize.Op;
-// const users = db.User;
-
-// let registerController = {
-//     index: function(req, res){
-        
-//         return res.render('register');
-//     },
-//     store: function(req, res){ 
-        
-//         let user = {
-//            name : req.body.name,
-//            email: req.body.email,
-//            password: bcrypt.hashSync(req.body.password, 10), 
-//        }
-//        users.create(user)
-//        .then( user => {
-//         return res.redirect('/login')
-//        })
-//        .catch(e => {console.log(e)});
-
-//     }
-// }
-
-// module.exports = registerController;
 
 const bcrypt = require('bcryptjs');
 const db = require('../database/models');
