d7e9fdd702e25c2afe72fddf80c44bacc524fda3
micaela160 <msalas@udesa.edu.ar>
2021-06-15 18:08:25 -0300

    mica

--- database/models/Usuarios.js
+++ database/models/Usuarios.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, dataTypes){
 
     //Definir un alias.
-    let alias = 'Usarios'; //Con este alias sequelize va a identificar internamente al archivo de modelo.
+    let alias = 'Usuarios'; //Con este alias sequelize va a identificar internamente al archivo de modelo.
 
     //Describir la configuración de las columnas de la tabla
     let cols = {
