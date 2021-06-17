991ef677016347e29b8efe4be666b5e44e8bf504
magacarolina <mparra@udesa.edu.ar>
2021-06-17 00:49:00 -0300

    mer

--- database/models/Productos.js
+++ database/models/Productos.js
@@ -30,7 +30,7 @@ module.exports = function(sequelize, dataTypes){
 
     let config = {
         tableName: 'productos', 
-        timestamps: false, //Si la tabla no tiene los campos created_at y updated_at
+        timestamp: false, //Si la tabla no tiene los campos created_at y updated_at
         underscored: false, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.        
     }
 
