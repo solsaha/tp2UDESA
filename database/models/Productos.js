c28b069ec39ed09d3b9986c5d824e53de1d27532
magacarolina <mparra@udesa.edu.ar>
2021-06-19 20:47:44 -0300

    se

--- database/models/Productos.js
+++ database/models/Productos.js
@@ -31,7 +31,7 @@ module.exports = function(sequelize, dataTypes){
     let config = {
         tableName: 'productos', 
         timestamp: true, //Si la tabla no tiene los campos created_at y updated_at
-        underscored: true, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.        
+        underscored: false, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.        
     }
 
    const Productos = sequelize.define(alias, cols, config);
