0a616233b3c97fa29458ab1fccb22dec7349169d
solsaha <ssaha@udesa.edu.ar>
2021-06-24 16:59:45 -0300

    modelo

--- database/models/Usuarios.js
+++ database/models/Usuarios.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, dataTypes){
         nombre:{
             type: dataTypes.STRING,
         },
-        fechaDeNacimiento:{
+        fecha_nacimiento:{
             type: dataTypes.DATE,
         },
     
